e3d946fe6fa8fd10f4a84bd71cf05807ec556d5e
Derek Hampton <derek.hampton@augmensis.co.uk>
2014-05-21 13:19:04 +0000

    Progress 21/05/14

--- app/models/zoopla.rb
+++ app/models/zoopla.rb
@@ -1,27 +1,58 @@
 class Zoopla
   include HTTParty
   base_uri 'api.zoopla.co.uk/api/v1/'
-  default_params :output => 'json', :api_key => '4zygpw9taptacn88vwkrvtka', :radius => '1', :page_size => '1', :listing_status => 'sale', :minimum_price => '10000'
+  default_params :output => 'json', :api_key => '4zygpw9taptacn88vwkrvtka', :radius => 5, :page_size => 10, :listing_status => "sale", :minimum_price => 100000, :maximum_price => 10000000
   format :json
   
+  
+  
   def self.find_local_properties(postcode)
+    @property_id = Random.rand(0...9)
     response = get('property_listings.json', :query => {:postcode => postcode})
     if response.success?
-      response
+      @property_data = response
     else
       raise response.response
     end
   end
   
-  def self.find_local_property_image(postcode)
-    response = get('property_listings.json', :query => {:postcode => postcode})
-    if response.success?
-      response["listing"][0]["image_url"]
+  def self.find_local_property_image
+    if @property_data != nil
+      @property_data["listing"][@property_id]["image_url"]
     else
-      raise response.response
+      raise @property_data.body
     end
   end
   
+  def self.find_local_property_price
+    if @property_data != nil
+       @property_data["listing"][@property_id]["price"].to_i
+    else
+      raise @response.body
+    end
+  end
   
+  def self.calculate_random_price
+    if find_local_property_price != 0
+      randprice = rand_price(find_local_property_price)
+    else
+      randprice = "POA"
+    end
+  end
+  
+  def self.rand_price(seed)  
+    hi = seed + percent(seed, 0.3)
+    lo = seed - percent(seed, 0.3)
+    result = (Random.rand(lo...hi) / 1000).round * 1000
+    
+  end
+  
+  def self.percent(seed, base)
+    if seed != 0
+      result = seed * base
+    else
+      seed
+    end
+  end
   
 end
