12d781e5b23c8d9ecc8e33b5f71fe377b6e8cc33
Derek Hampton <derek.hampton@augmensis.co.uk>
2014-05-23 13:17:30 +0000

    optimised code, randomness + number format

--- app/models/zoopla.rb
+++ app/models/zoopla.rb
@@ -1,13 +1,15 @@
-class Zoopla
+class Zoopla 
+  @pagesize = 100
   include HTTParty
   base_uri 'api.zoopla.co.uk/api/v1/'
-  default_params :output => 'json', :api_key => '4zygpw9taptacn88vwkrvtka', :radius => 5, :page_size => 10, :listing_status => "sale", :minimum_price => 100000, :maximum_price => 10000000
+  default_params :output => 'json', :api_key => '4zygpw9taptacn88vwkrvtka', :radius => 5, :page_size => @pagesize, :listing_status => "sale", :minimum_price => 100000, :maximum_price => 10000000
   format :json
   
+  @pagesize
   
   
   def self.find_local_properties(postcode)
-    @property_id = Random.rand(0...9)
+    @property_id = Random.rand(0...(@pagesize-1))
     response = get('property_listings.json', :query => {:postcode => postcode})
     if response.success?
       @property_data = response
@@ -26,7 +28,7 @@ class Zoopla
   
   def self.find_local_property_price
     if @property_data != nil
-       @property_data["listing"][@property_id]["price"].to_i
+      @property_data["listing"][@property_id]["price"].to_i
     else
       raise @response.body
     end
@@ -40,9 +42,10 @@ class Zoopla
     end
   end
   
-  def self.rand_price(seed)  
-    hi = seed + percent(seed, 0.3)
-    lo = seed - percent(seed, 0.3)
+  def self.rand_price(seed) 
+    base = 0.3
+    hi = seed + percent(seed, base)
+    lo = seed - percent(seed, base)
     result = (Random.rand(lo...hi) / 1000).round * 1000
     
   end
--- app/views/houses/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @house, :id, :location, :realprice, :fakeprice1, :fakeprice2, :created_at, :updated_at
